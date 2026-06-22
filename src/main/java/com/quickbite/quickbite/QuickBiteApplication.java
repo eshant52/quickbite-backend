3556c0448d58589659ea9e415446ae71591bd27a
Eshant Nandkishor Sonune <69902492+eshant52@users.noreply.github.com>
2026-06-22 15:17:49 +0530

    updated code

--- src/main/java/com/quickbite/quickbite/QuickBiteApplication.java
+++ src/main/java/com/quickbite/quickbite/QuickBiteApplication.java
@@ -2,12 +2,14 @@ package com.quickbite.quickbite;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class QuickBiteApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(QuickBiteApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(QuickBiteApplication.class, args);
+    }
 
 }
